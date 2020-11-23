9a352fceaf33986c0ce8e8042fe5325c4bf9a47b
irma <marchukidze@gmail.com>
2020-11-23 20:31:43 +0200

    js commit

---
+++ main.js
@@ -0,0 +1,28 @@
+let n = +prompt('Введите количество чисел', '');
+let a = 1;
+let b = 1;
+let c;
+
+if (n == 1) {
+    console.log(a);
+} else {
+    console.log(a);
+    console.log(b);
+}
+
+for (i = 3; i <= n; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+    console.log(b);
+}
+
+
+
+
+
+
+
+
+
+
